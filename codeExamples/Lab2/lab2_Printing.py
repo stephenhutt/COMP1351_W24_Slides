8588e76385f91c0c47be30dd079170b8e540e772
Stephen Hutt <39675459+stephenhutt@users.noreply.github.com>
2024-01-10 17:16:45 -0700

    Lab2 Update

---
+++ codeExamples/Lab2/lab2_Printing.py
@@ -0,0 +1,11 @@
+myNumber = 5
+print("my Number is ", myNumber, myNumber, myNumber)
+
+#Get User Input
+userInput = input("Enter a Number:")
+
+#Convert Inout to an integer
+userInputInteger = int(userInput)
+
+#Print, combine strings and ints
+print("You Entered:", userInput)
